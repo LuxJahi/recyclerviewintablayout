eb7fc97cb8356dacc687473c62182861b0e03cdc
LuxJahi <ojstewart@hotmail.com>
2016-12-16 19:37:10 -0800

    Initial commit

---
+++ app/src/main/java/pwnavor/recyclerviewintablayout/FeedItem.java
@@ -0,0 +1,52 @@
+package pwnavor.recyclerviewintablayout;
+
+/**
+ * Created by Jahi on 12/16/2016.
+ */
+public class FeedItem {
+    String title;
+    String link;
+    String pubDate;
+    String description;
+    String thumbnailUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
